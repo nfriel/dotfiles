99ccc8d3c02a5d769d9e366a1db962363276c524
Nathan Friel <nathanfriel97@gmail.com>
2025-11-20 14:36:51 -0500

    added tab shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,12 +10,15 @@ vim.o.softtabstop = -1
 vim.o.expandtab = true
 
 -- keymaps
-local telescope = require('telescope.builtin')
 vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>tt', ':tabNext<cr>')
+vim.keymap.set('n', '<leader>tn', ':tabnew .<cr>')
+-- telescope
+local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>w', ':write<cr>')
 vim.keymap.set('n', '<leader>ff', telescope.find_files)
 vim.keymap.set('n', '<leader>fg', telescope.live_grep)
 vim.keymap.set('n', '<leader>fb', telescope.buffers)
 
 -- lsp
-vim.lsp.enable({'clangd', 'lua_ls', 'rust_analyzer', 'pylsp'})
+vim.lsp.enable({'clangd', 'lua_ls', 'rust_analyzer', 'pylsp', 'ocamllsp'})
